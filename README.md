8d481769358c7f074371aa7a90aa6c699db8c928
Avery Brooks <averyjamesbrooks@gmail.com>
2014-11-10 19:10:25 -0500

    tab out py and fix some syntax

---
+++ README.md
@@ -0,0 +1,10 @@
+#keepgrabbing.py#
+
+This is a transcription of the Python script which Aaron Swartz used to download a large number of documents from JSTOR archive between 2010 and 2011.
+
+I'm not sure what Aaron would have wanted us to do with this code, but my instinct is that he'd want it freely available, and it's worth having in an executable machine readable format under version control, rather than on a hard drive somewhere which has long since stopped spinning.  I guess this is sort of a memorial in some sense.
+
+Rest in peace.
+
+##Todo##
+* Line 5 contains a redacted hostname/domain, does anyone know what that was?
--- keepgrabbing.py
+++ keepgrabbing.py
@@ -2,21 +2,20 @@
 import subprocess, urllib, random
 class NoBlocks(Exception): pass
 def getblocks():
-r = urllib.urlopen("http://{?REDACTED?}/grab").read()
-if '<html' in r.lower(): raise NoBlocks
-return r.split()
+    r = urllib.urlopen("http://{?REDACTED?}/grab").read()
+    if '<html' in r.lower(): raise NoBlocks
+    return r.split()
 
 import sys
 if len(sys.argv) > 1:
-  prefix = ['--socks5', sys.argv[1]]
-else
-  prefix = ['']
+    prefix = ['--socks5', sys.argv[1]]
+else:
+    prefix = ['']
 
 line = lambda x: ['curl'] + prefix + ['-H', "Cookie: TENACIOUS=" + str(random.random())[3:], '-o', 'pdfs/' + str(x) + '.pdf', "http://www.jstor.org/stable/pdfplus/" + str(x) + ".pdf?acceptTC=true"]
 
 while 1:
-blocks = getblocks()
-for block in blocks:
-  print block
-  subprocess.Popen(line(block)).wait()
-  
+    blocks = getblocks()
+    for block in blocks:
+        print block
+        subprocess.Popen(line(block)).wait()
